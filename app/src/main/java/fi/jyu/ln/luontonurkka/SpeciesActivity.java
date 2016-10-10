0ff13b564f878697788b4f8eaba0329b766a4301
Jarno <jarnokie@gmail.com>
2016-10-10 17:28:08 +0300

    Renamed and removed "email" button

--- app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SpeciesView extends AppCompatActivity {
+public class SpeciesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
