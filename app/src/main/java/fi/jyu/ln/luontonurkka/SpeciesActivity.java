33aff980a0f56c056cc7b8e923596b81b7bf1a77
Jarno <jarnokie@gmail.com>
2016-10-10 18:07:59 +0300

    SpeciesActivity test image

--- app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
@@ -1,12 +1,20 @@
 package fi.jyu.ln.luontonurkka;
 
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import static fi.jyu.ln.luontonurkka.R.id.species_content_text;
+import static fi.jyu.ln.luontonurkka.R.id.species_toolbar_img;
 import static fi.jyu.ln.luontonurkka.R.id.species_toolbar_layout;
 
 /**
@@ -35,5 +43,20 @@ public class SpeciesActivity extends AppCompatActivity {
         // get text from desc
         TextView contentTextView = (TextView)this.findViewById(species_content_text);
         contentTextView.setText(species.getDescr());
+
+        // get img
+        ImageView imgView = (ImageView)this.findViewById(species_toolbar_img);
+        imgView.setImageResource(R.drawable.kissa);
+    }
+
+    private static Drawable LoadImageFromUrl(String url) {
+        try {
+            InputStream is = (InputStream)new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "img");
+            return d;
+        } catch(Exception e) {
+            Log.w("SpeciesAcitivty", "Image " + url + " load failed");
+            return null;
+        }
     }
 }
