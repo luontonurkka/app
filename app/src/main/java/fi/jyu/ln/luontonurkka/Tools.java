e6dbde343897eb468b11833a2a0312cf61c1687b
Jarno <jarnokie@gmail.com>
2016-10-11 16:58:02 +0300

    Tools class and image loading class

---
+++ app/src/main/java/fi/jyu/ln/luontonurkka/Tools.java
@@ -0,0 +1,51 @@
+package fi.jyu.ln.luontonurkka;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by jarno on 10/11/16.
+ */
+
+public class Tools {
+
+    /**
+     * Class that loads and image from url and sets it in the ImageView provided.
+     *
+     * Usage:
+     * new DownloadImageTask(imageView).execute("http://url.com/img.png");
+     */
+    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        private ImageView imgView;
+
+        public DownloadImageTask(ImageView imgView) {
+            this.imgView = imgView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap bitmap = null;
+            String url = urls[0];
+            try {
+                InputStream in = new URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+
+        protected void onPostExecute(Bitmap result) {
+            imgView.setImageBitmap(result);
+        }
+    }
+
+}
