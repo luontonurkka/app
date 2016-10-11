2134b510d2bd7d3783fc6caf00e7857fd130bc25
Jarno <jarnokie@gmail.com>
2016-10-11 17:58:23 +0300

    Single interface for task completion

--- app/src/main/java/fi/jyu/ln/luontonurkka/tools/OnTaskCompleted.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/tools/OnTaskCompleted.java
@@ -1,10 +1,13 @@
 package fi.jyu.ln.luontonurkka.tools;
 
+import android.graphics.Bitmap;
+
 /**
  * Interface for DownloadTextTask
  *
  * Created by jarno on 10/11/16.
  */
-public interface OnTextTaskCompleted {
+public interface OnTaskCompleted {
     void onTaskCompleted(String result);
+    void onTaskCompleted(Bitmap result);
 }
