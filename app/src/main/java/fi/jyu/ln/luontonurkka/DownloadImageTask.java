3ebf4bdcb836380b4f47a1680fb37f465946f4dd
Jarno <jarnokie@gmail.com>
2016-10-11 17:09:21 +0300

    Indentation

--- app/src/main/java/fi/jyu/ln/luontonurkka/DownloadImageTask.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/DownloadImageTask.java
@@ -13,39 +13,35 @@ import java.net.URL;
  * Created by jarno on 10/11/16.
  */
 
-public static class Tools {
+/**
+ * Class that loads and image from url and sets it in the ImageView provided.
+ *
+ * Usage:
+ * new DownloadImageTask(imageView).execute("http://url.com/img.png");
+ */
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
-    /**
-     * Class that loads and image from url and sets it in the ImageView provided.
-     *
-     * Usage:
-     * new DownloadImageTask(imageView).execute("http://url.com/img.png");
-     */
-    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    private ImageView imgView;
 
-        private ImageView imgView;
-
-        public DownloadImageTask(ImageView imgView) {
-            this.imgView = imgView;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            Bitmap bitmap = null;
-            String url = urls[0];
-            try {
-                InputStream in = new URL(url).openStream();
-                bitmap = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return bitmap;
-        }
-
-
-        protected void onPostExecute(Bitmap result) {
-            imgView.setImageBitmap(result);
-        }
+    public DownloadImageTask(ImageView imgView) {
+        this.imgView = imgView;
     }
 
+    protected Bitmap doInBackground(String... urls) {
+        Bitmap bitmap = null;
+        String url = urls[0];
+        try {
+            InputStream in = new URL(url).openStream();
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+    protected void onPostExecute(Bitmap result) {
+        imgView.setImageBitmap(result);
+    }
 }
