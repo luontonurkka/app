cd41b248c9ddc32d6077e9e7f45a10369c300752
Jarno <jarnokie@gmail.com>
2016-10-22 20:29:54 +0300

    Removed settings oncreate method

--- app/src/main/java/fi/jyu/ln/luontonurkka/TabbedListActivity.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/TabbedListActivity.java
@@ -234,13 +234,6 @@ public class TabbedListActivity extends AppCompatActivity implements NavigationV
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
