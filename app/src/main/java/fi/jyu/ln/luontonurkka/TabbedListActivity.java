7e54e738491a581b43561040562831e4dc96d6a2
Jarno Kiesiläinen <jarnokie@gmail.com>
2016-10-26 10:26:18 +0300

    Missing menu id commented

--- app/src/main/java/fi/jyu/ln/luontonurkka/TabbedListActivity.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/TabbedListActivity.java
@@ -245,7 +245,7 @@ public class TabbedListActivity extends AppCompatActivity implements NavigationV
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings, menu);
+        //getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
 
