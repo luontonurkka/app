22b684d4b93f535a1a1c16fa46c85f47cdb9c188
Jarno <jarnokie@gmail.com>
2016-11-18 11:46:20 +0200

    Ids wrongway around

--- app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
+++ app/src/main/java/fi/jyu/ln/luontonurkka/SpeciesActivity.java
@@ -3,34 +3,22 @@ package fi.jyu.ln.luontonurkka;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Iterator;
-
 import fi.jyu.ln.luontonurkka.tools.DownloadImageTask;
-import fi.jyu.ln.luontonurkka.tools.DownloadTextTask;
 import fi.jyu.ln.luontonurkka.tools.OnTaskCompleted;
 import fi.jyu.ln.luontonurkka.tools.WikiFetcher;
 
-import static fi.jyu.ln.luontonurkka.R.id.species_content_text;
-import static fi.jyu.ln.luontonurkka.R.id.species_toolbar_img;
 import static fi.jyu.ln.luontonurkka.R.id.species_toolbar_layout;
 
 /**
@@ -63,24 +51,23 @@ public class SpeciesActivity extends AppCompatActivity {
         layout.setExpandedTitleColor(Color.WHITE);
         layout.setCollapsedTitleTextColor(Color.WHITE);
 
-        String id = species.getIdFin();
+        // TODO ids wrongway around
+        String id = species.getIdEng();
         String lang = "fi";
         if(id.length() < 1) {
-            id = species.getIdEng();
+            id = species.getIdFin();
             lang = "en";
         }
 
         OnTaskCompleted task = new OnTaskCompleted() {
             @Override
             public void onTaskCompleted(String result) {
-                Log.d(getClass().toString(), "TEXT");
                 if(result != null)
                     setTextComplete(result);
             }
 
             @Override
             public void onTaskCompleted(Bitmap result) {
-                Log.d(getClass().toString(), "IMG");
                 if(result != null)
                     setImgComplete(result);
             }
